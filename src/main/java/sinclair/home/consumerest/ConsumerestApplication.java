29092ea4ade45f510dcfe893f714d8ff4bb1270a
Kyle Sinclair <Kyle.dean.sinclair@gmail.com>
2020-12-21 14:00:29 +0100

    Initial commit

---
+++ src/main/java/sinclair/home/consumerest/ConsumerestApplication.java
@@ -0,0 +1,13 @@
+package sinclair.home.consumerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerestApplication.class, args);
+    }
+
+}
